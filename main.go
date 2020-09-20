926ee123369dc282942e638ce057dcfd4ebfb132
Jonas Fronchetti <jonasfronchetti@gmail.com>
2020-09-20 14:40:04 -0300

    Adicionado

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Developers!!!!</h1><h2>Galou geral hein!</h2>")
+	fmt.Fprintf(w, "<h1>Hello Developers!!!!</h1><h2>Galou geral hein- foi!</h2>")
 }
